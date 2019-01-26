dbdaf6ff2c5bd28ea223a0d30d28a519251f67c9
Billy Witherspoon <wwither@gmail.com>
2019-01-26 01:46:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+my_new_hash = {}
+  languages.each do |style_name, sub_languages|
+    sub_languages.each do |name, type_data|
+    #binding.pry
+      if my_new_hash[:name]
+      else
+        my_new_hash[:name] = {style: [style_name]}
+        #binding.pry
+      end
+    end
+  end
+binding.pry
 end
